058afc9105b87f7509a050e23b4cdb169d5f5e55
KovtunIKM724a <yevhen.kovtun@infiz.khpi.edu.ua>
2025-03-28 09:44:04 +0200

    Добавьте файлы проекта.

---
+++ lab11_2/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+	double* pa, * pb;
+	pa = new double;
+	pb = new double;
+
+
+	double a, b;
+	cout << "Enter a value in double variable a: ";
+	cin >> a;
+	cout << "Enter a value in double variable b: ";
+	cin >> b;
+
+	pa = &a;
+	pb = &b;
+
+	*pa *= 2;
+
+	cout << "An address of var a = " << pa << endl;
+	cout << "A value of var a = " << *pa << endl;
+	cout << "An address of var b = " << pb << endl;
+	cout << "A value of var b = " << *pb << endl;
+
+	delete pa;
+	delete pb;
+}
